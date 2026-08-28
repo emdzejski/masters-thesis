ccf1401756c46b84f24e0830b46e0b2fc41872c1
emdzejski <mateusz.janus48@gmail.com>
2026-08-28 17:03:39 +0200

    added loop through planes, saving image and canvas configuration

--- 2d_viewer.py
+++ 2d_viewer.py
@@ -1,30 +1,52 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from pathlib import Path
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+#from matplotlib.ticker import FuncFormatter
 
-f_path = "/home/jpet/ccb_dane/offbeam_no_plexa/no_plexi_recoim_noAtt/no_plexi_recoim_noAtt_it5.img"
+voxel_size = 2.5
+f_path = "/home/jpet/ccb_dane/offbeam_plexa/plexi_recoim_noAtt/plexi_recoim_noAtt_it5.img"
 f_name = Path(f_path).stem
 
 
 n_voxels = [160,160, 200]
 l_y = 0
 r_y = 160
-title_label = {0:"YZ", 1:"XZ", 2:"XY"}
+axes_label = {0:"zy", 1:"zx", 2:"yx"}
 
 vol = np.fromfile(f_path, dtype=np.float32)
 vol = np.reshape(vol, n_voxels, order='F')
 vol_y_flipped = np.flip(vol,1)
 
-vol_fov = vol_y_flipped[::, l_y:r_y, ::]
-ax = 0
+vol_fov = vol_y_flipped[20:140, 20:110, 50:140]
 
-projection_2d = np.sum(vol_fov, axis = ax) #sum along a given axis to create a plane projection 
 
-projection_2d = projection_2d.T
 
-plt.figure(figsize=(5.5, 6))
-plt.imshow(projection_2d, cmap='turbo')
-plt.colorbar(label="")
-plt.title(f"{title_label[ax]} plane of {f_name}")
-plt.tight_layout()
-plt.show()
+#ax = 0
+#fig, ax = plt.subplots()
+axes = [0,1,2]
+for a in axes:
+    projection_2d = np.sum(vol_fov, axis = a) #sum along a given axis to create a plane projection 
+
+    projection_2d = projection_2d.T
+
+    h,w = projection_2d.shape[:2]
+    extent = [0, w * voxel_size, h * voxel_size, 0]
+
+    #plt.figure(figsize=(5.5, 6))
+    fig, ax = plt.subplots(figsize=(8, 6))
+    #ax.imshow(projection_2d, cmap='turbo', extent = extent)
+    im = ax.imshow(projection_2d, cmap='turbo', extent = extent, aspect= 'auto')
+    #fig.colorbar(im, ax=ax, label="")
+    #ax.set_title(f"{title_label[a]} plane of {f_name}")
+    ax.set_xlabel(f"{axes_label[a][1]} [mm]")
+    ax.set_ylabel(f"{axes_label[a][0]} [mm]")
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.15)
+    fig.colorbar(im, cax=cax)
+   
+    plt.tight_layout()
+    plt.savefig(f"/home/jpet/ccb_dane/graphs/reco_ims/plexi_noatt/{axes_label[a]}_{f_name}.png", dpi=300)
+    plt.show()
+    plt.close()
